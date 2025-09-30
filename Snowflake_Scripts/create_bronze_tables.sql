312a14feac6b5ef3c97c19058289c0d694832308
Ahmed Badawy <142052692+AhmedB5@users.noreply.github.com>
2025-09-30 16:17:55 +0300

    Update create_bronze_tables.sql

--- Snowflake_Scripts/create_bronze_tables.sql
+++ Snowflake_Scripts/create_bronze_tables.sql
@@ -1,1 +1,29 @@
+-- CREATE BRONZE TABLES
+
+CREATE OR REPLACE TABLE bronze.CardBase(
+    Card_Number VARCHAR,
+    Card_Family VARCHAR, 
+    Credit_limit INTEGER,
+    Cust_ID VARCHAR
+); 
+
+CREATE OR REPLACE TABLE bronze.CustomerBase(
+    Cust_ID VARCHAR,
+    Age INTEGER, 
+    Customer_Segment VARCHAR,
+    Customer_Vintage_Group VARCHAR
+);
+
+CREATE OR REPLACE TABLE bronze.TransactionBase(
+    Transaction_ID VARCHAR, 
+    Transaction_Date DATE,
+    Credit_Card_ID VARCHAR, 
+    Transaction_Value INTEGER,
+    Transaction_Segment VARCHAR
+);
+
+CREATE OR REPLACE TABLE bronze.FraudBase(
+    Transaction_ID VARCHAR, 
+    Fraud_Flag INTEGER
+);
 
