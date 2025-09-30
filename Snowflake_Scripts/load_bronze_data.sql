760c1e1197f25bcb5af61478fe97f24e0684ea50
Ahmed Badawy <142052692+AhmedB5@users.noreply.github.com>
2025-09-30 16:19:30 +0300

    Update load_bronze_data.sql

--- Snowflake_Scripts/load_bronze_data.sql
+++ Snowflake_Scripts/load_bronze_data.sql
@@ -1,1 +1,18 @@
+-- LOAD DATA INTO BRONZE TABLES
+
+COPY INTO bronze.CardBase
+FROM @bronze.my_stage/CardBase.csv
+FILE_FORMAT = (TYPE = CSV SKIP_HEADER = 1);
+
+COPY INTO bronze.CustomerBase
+FROM @bronze.my_stage/CustomerBase.csv
+FILE_FORMAT = (TYPE = CSV SKIP_HEADER = 1);
+
+COPY INTO bronze.TransactionBase
+FROM @bronze.my_stage/TransactionBase.csv
+FILE_FORMAT = (TYPE = CSV SKIP_HEADER = 1);
+
+COPY INTO bronze.FraudBase
+FROM @bronze.my_stage/FraudBase.csv
+FILE_FORMAT = (TYPE = CSV SKIP_HEADER = 1);
 
